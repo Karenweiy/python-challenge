75e8ae0317728716bf95a2c83779cc890d91bc4b
Karen Wei <karenweiy@gmail.com>
2019-06-08 14:13:56 -0400

    HW3

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,34 +1,40 @@
 import os
 import csv
 
-#import data from resources and calculate the count
 csvpath = os.path.join('Resources','budget_data.csv')
 
 month=[]
-pro_loss=[]
+profit=[]
+month_profit_change=[]
 
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile,delimiter=',')
-    print (csvreader)
+   # print (csvreader)
     csv.header = next(csvreader)
    
-
     for row in csvreader:
         month.append(str(row[0]))
-        pro_loss.append(int(row[1]))
+        profit.append(int(row[1]))
         
+for i in range(len(profit)-1):
+    #print(i)
+    #print(len(profit))
+   # print(profit[i])
+
+    month_change=profit[i+1]-profit[i]
+    month_profit_change.append(month_change)
   
-    print("Financial Analysis")
-    print("Total Months:" + str(len(month)))
-    print("Total profit or loss:" + str(sum(pro_loss)))
-    print(sum(pro_loss)/len(month))
-    print(max(pro_loss))
-    print(min(pro_loss))
+print("Financial Analysis")
+print("Total Months:" + str(len(month)))
+print("Total profit or loss:" + str(sum(profit)))
+print(sum(month_profit_change)/(len(month)-1))
+print(max(month_profit_change))
+print(min(month_profit_change))
 
 out = open("budget_summ.txt","w+")
 
 out.write("Total Months: " + str(len(month))+"\n")
-out.write("Total Profits: $" + str(sum(pro_loss))+"\n")
-out.write("Average Profits: $" + str(sum(pro_loss)/len(month))+"\n")
-out.write("Maximum Profits: $" + str(max(pro_loss))+"\n")
-out.write("Minimum Profits: $" + str(min(pro_loss))+"\n")
+out.write("Total Profits: $" + str(sum(profit))+"\n")
+out.write("Average Profits: $" + str(sum(month_profit_change)/len(month))+"\n")
+out.write("Maximum Profits: $" + str(max(month_profit_change))+"\n")
+out.write("Minimum Profits: $" + str(min(month_profit_change))+"\n")
